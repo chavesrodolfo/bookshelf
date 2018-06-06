72a85166160c6b87488f932b68bbd7940a38a575
unknown <chavesrodolfo@gmail.com>
2018-06-06 13:40:38 -0300

    web location

--- books-app/js/app.js
+++ books-app/js/app.js
@@ -1,19 +1,22 @@
-//var BASE_URL = 'http://localhost:8080';
 var BASE_URL = 'http://books-api-books.a3c1.starter-us-west-1.openshiftapps.com';
 
 var booksApp = angular.module('booksApp', [
-	"ngRoute",
-	"dashboard"
-]); 
+  "ngRoute",
+  "dashboard"
+]);
 
-booksApp.config(['$locationProvider' ,'$routeProvider',
-    function config($locationProvider, $routeProvider) {
-      $locationProvider.hashPrefix('!');
+booksApp.config(['$locationProvider', '$routeProvider',
+  function config($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('!');
 
-      $routeProvider.
-        when('/dashboard', {
-          template: '<dashboard></dashboard>'
-        }).
-        otherwise('/dashboard');
+    if (window.location.href.includes('localhost')) {
+      BASE_URL = 'http://localhost:8080';
     }
+
+    $routeProvider.
+      when('/dashboard', {
+        template: '<dashboard></dashboard>'
+      }).
+      otherwise('/dashboard');
+  }
 ]);
